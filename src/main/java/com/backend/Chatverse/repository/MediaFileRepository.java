aa5a609aaa50dcefd2da6d27cfeb5da226d46ba4
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-25 17:21:24 +0200

    initial-chatroomServceImpl-commit

--- src/main/java/com/backend/Chatverse/repository/MediaFileRepository.java
+++ src/main/java/com/backend/Chatverse/repository/MediaFileRepository.java
@@ -1,8 +1,9 @@
 package com.backend.Chatverse.repository;
 
+import com.backend.Chatverse.model.MediaFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MediaFileRepository extends JpaRepository <MediaFileRepository, Long> {
+public interface MediaFileRepository extends JpaRepository <MediaFile, Long> {
 }
--- src/main/java/com/backend/Chatverse/service/ChatRoomService.java
+++ src/main/java/com/backend/Chatverse/service/ChatRoomService.java
@@ -1,4 +1,13 @@
 package com.backend.Chatverse.service;
 
+import com.backend.Chatverse.model.ChatRoom;
+
+import java.util.List;
+
 public interface ChatRoomService {
+    List<ChatRoom> chatRoomList();
+    ChatRoom getChatRoomById(Long id);
+    ChatRoom saveChatRoom(ChatRoom chatRoom, Long userId);
+    ChatRoom updateChatRoom(Long id, ChatRoom chatRoom);
+    void removeChatRoom (Long id);
 }
