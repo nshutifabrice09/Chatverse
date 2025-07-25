979e546e01bebe8ac4424044e745cf6ca6f1a39a
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-25 10:36:21 +0200

    final-repo-commit

--- src/main/java/com/backend/Chatverse/repository/MediaFileRepository.java
+++ src/main/java/com/backend/Chatverse/repository/MediaFileRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MediaFile extends JpaRepository <MediaFile, Long> {
+public interface MediaFileRepository extends JpaRepository <MediaFileRepository, Long> {
 }
--- src/main/java/com/backend/Chatverse/repository/MessageRepository.java
+++ src/main/java/com/backend/Chatverse/repository/MessageRepository.java
@@ -1,2 +1,9 @@
-package com.backend.Chatverse.repository;public interface MessageRepository {
+package com.backend.Chatverse.repository;
+
+import com.backend.Chatverse.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository <Message, Long> {
 }
