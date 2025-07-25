07229e17a0919d273780775b4887f46228814081
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-25 10:35:18 +0200

    updates-repo-commit

--- src/main/java/com/backend/Chatverse/repository/MediaFileRepository.java
+++ src/main/java/com/backend/Chatverse/repository/MediaFileRepository.java
@@ -1,9 +1,8 @@
 package com.backend.Chatverse.repository;
 
-import com.backend.Chatverse.model.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatRoomRepository extends JpaRepository <ChatRoom, Long> {
+public interface MediaFile extends JpaRepository <MediaFile, Long> {
 }
